cf5b8b8b2291b4a227bbe78cab4ee5d2cc0d8e0f
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2017-12-09 23:16:37 +0600

    Change Google endpoint url to universal

--- server/modules/get_images_url.py
+++ server/modules/get_images_url.py
@@ -14,7 +14,7 @@ def get_images(query, max_images=100):
 	query= query.split()
 	query='+'.join(query)
 
-	url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+	url="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch"
 
 	header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 	soup = get_soup(url,header)
