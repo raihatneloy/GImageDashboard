83a38f39b18941228780b929c9d128ec56e3e4fa
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2017-12-17 20:35:29 +0600

    Fix - flickr multiple word search

--- server/modules/flickr.py
+++ server/modules/flickr.py
@@ -1,11 +1,20 @@
-import os
+from bs4 import BeautifulSoup
+import requests
+import urllib2
 import json
 
+
 class Flickr:
+    def get_soup(self, url, header):
+        return BeautifulSoup(urllib2.urlopen(urllib2.Request(url, headers=header)), 'html.parser')
+
     def get_flickr_users(self, keyword):
+        keyword = keyword.split()
+        keyword = '%20'.join(keyword)
         url = 'https://www.flickr.com/search/people/?username=%s' % keyword
 
-        html = os.popen('curl "%s"' % url).read()
+        header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+        html = self.get_soup(url, header).text
         index1 = html.find('{"_data":[{') + 9
         data = html[index1:]
         index2 = data.find('"}]')+2
